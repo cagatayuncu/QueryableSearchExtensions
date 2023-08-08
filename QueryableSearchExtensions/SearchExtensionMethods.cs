85a1eb521c7dea7fb2fbc5c8277e1e36f55a3d7d
Çağatay Uncu <cagatay.uncu@hotmail.com>
2023-08-08 15:53:06 +0200

    add new method

--- QueryableSearchExtensions/SearchExtensionMethods.cs
+++ QueryableSearchExtensions/SearchExtensionMethods.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore;
 
 namespace QueryableSearchExtensions
 {
     public static class SearchExtensionMethods
     {
-        public static IQueryable<T> GlobalSearchQuery<T>(this DbSet<T> dbSet, string searchTerm,  Expression<Func<T, string>>[] propertySelectors) where T : class
+        public static IQueryable<T> PropertySearchQuery<T>(this DbSet<T> dbSet, string searchTerm, params Expression<Func<T, string>>[] propertySelectors) where T : class
         {
             IQueryable<T> query = dbSet;
 
@@ -47,6 +47,43 @@ namespace QueryableSearchExtensions
 
             return query.Where(expression);
         }
+        public static IQueryable<T> GlobalSearcshQuery<T>(this DbSet<T> dbSet, string searchTerm) where T : class
+        {
+            IQueryable<T> query = dbSet;
+
+            List<Expression> expressions = new List<Expression>();
+
+            ParameterExpression parameter = Expression.Parameter(typeof(T), "p");
+
+            MethodInfo containsMethod = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+            foreach (PropertyInfo prop in typeof(T).GetProperties().Where(x => x.PropertyType == typeof(string)))
+            {
+                MemberExpression memberExpression = Expression.PropertyOrField(parameter, prop.Name);
+
+                ConstantExpression valueExpression = Expression.Constant(searchTerm, typeof(string));
+
+                MethodCallExpression containsExpression = Expression.Call(memberExpression, containsMethod, valueExpression);
+
+                expressions.Add(containsExpression);
+            }
+
+            if (expressions.Count == 0)
+                return query;
+
+            Expression orExpression = expressions[0];
+
+            for (int i = 1; i < expressions.Count; i++)
+            {
+                orExpression = Expression.OrElse(orExpression, expressions[i]);
+            }
+
+            Expression<Func<T, bool>> expression = Expression.Lambda<Func<T, bool>>(
+                orExpression, parameter);
+
+            return query.Where(expression);
+        }
 
     }
+   
 }
